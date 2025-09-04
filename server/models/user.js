87a2c350aac661feef8282aa49340bbd0a0924e3
ashishh-dev <ashish.pandey@dody.dev>
2025-09-05 00:10:57 +0530

    added models

---
+++ server/models/user.js
@@ -0,0 +1,21 @@
+import express from "express"
+
+const user = new mongoose.Schema({
+    name:{
+         type: String,
+         required: ["Ashish"]
+
+    },
+    id:{
+
+    },
+    email:{
+
+    },
+    password:{
+
+    },
+})
+
+
+
